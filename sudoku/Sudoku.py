92683d584f310236b5aa09ab15b03447af6946a0
Amy <amychods@live.com>
2020-05-12 12:38:36 +0100

    Finished suduko solver

--- sudoku/Sudoku.py
+++ sudoku/Sudoku.py
@@ -1,4 +1,7 @@
 import numpy as np
+import time as t
+
+from IPython.display import clear_output
 
 
 class Sudoku:
@@ -6,6 +9,7 @@ class Sudoku:
     def __init__(self):
         self.grid = [[0]*9 for i in range(9)]
         self.grid = np.array(self.grid)
+        self.status = 'Initialised'
 
     def print_grid(self):
         print(self.grid)
@@ -14,15 +18,27 @@ class Sudoku:
         sep = '-------------------------'
         line = '| {0} {1} {2} | {3} {4} {5} | {6} {7} {8} |'
 
-        graph = sep
+        graph = ''
         for ii in range(3):
+            graph += '\n' + sep
             for jj in range(3):
                 row_i = ii*3 + jj
                 row = self.grid[row_i]
                 graph += '\n' + line.format(*row)
-            graph += '\n' + sep
+        graph += '\n' + sep
+        graph += '\n'
 
+        clear_output(wait=True)
         print(graph)
+        t.sleep(0.001)
+
+    @staticmethod
+    def print_ready():
+        print('Ready', end=" ")
+        for i in range(5):
+            print('.', end=" ")
+            t.sleep(1)
+        print(".")
 
     def initialise_game_1(self):
         row_1 = [5, 3, 0, 0, 7] + [0] * 4
@@ -37,6 +53,7 @@ class Sudoku:
         self.grid = np.array([row_1, row_2, row_3,
                               row_4, row_5, row_6,
                               row_7, row_8, row_9])
+        self.status = 'Ready'
 
     def possible(self, y, x, n):
         # Row
@@ -69,10 +86,28 @@ class Sudoku:
                             self.grid[y, x] = n
                             self.solve()
                             self.grid[y, x] = 0
-
                     return
 
-        print('Done!')
-        print('')
         self.print_grid_pretty()
-        input("More?")
+        input("Done! Anymore solutions?")
+
+    def solve_with_print(self):
+        for y in range(9):
+            for x in range(9):
+                if self.grid[y, x] == 0:
+                    for n in range(1, 10):
+                        if self.possible(y, x, n):
+                            # Fill in
+                            self.grid[y, x] = n
+                            self.print_grid_pretty()
+
+                            # Start again
+                            self.solve_with_print()
+
+                            # Back track
+                            self.grid[y, x] = 0
+                            self.print_grid_pretty()
+                    return
+
+        self.print_grid_pretty()
+        input("Done! Anymore solutions?")
--- sudoku/sudoku_sandbox.py
+++ sudoku/sudoku_sandbox.py
@@ -1,13 +1,9 @@
 # https://www.youtube.com/watch?v=G_UYXzGuqvM
 
-
 from sudoku.Sudoku import Sudoku
 
 s = Sudoku()
 s.initialise_game_1()
 s.print_grid_pretty()
-
-print('Ready')
-print('')
-
 s.solve()
+
